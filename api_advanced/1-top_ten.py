fd55b98bdd0d934ae0b54aefd57ee20ab4944b67
Chol1000 <c.monykuch@alustudent.com>
2024-10-20 16:33:47 -0700

    T

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    HEADERS = {"User-Agent": 'Mozilla/5.0'}
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
         HOT_POSTS = RESPONSE.json().get("data").get("children")
