6ffdd9d24286911f4539ca1519d06bfc68ba6f89
Chol1000 <c.monykuch@alustudent.com>
2024-10-20 16:14:55 -0700

    Corrected

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Prints the titles of the first 10 hot posts listed for a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit to query.
+    """
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
     
+    # Make the GET request to the Reddit API
+    RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+
     # Check if the subreddit exists
     if RESPONSE.status_code == 404:
-        print(None)
+        print(None)  # Non-existent subreddit
         return
     elif RESPONSE.status_code != 200:
-        print(None)
+        print(None)  # For any other error
         return
 
     # Parse the JSON response and print hot post titles
